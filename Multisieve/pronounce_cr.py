a8fa79f41b0a43ebf2788b310c52d683be725227
junzx <candnes@outlook.com>
2019-03-10 09:27:57 +0800

    去实验室前，改动了代词部分

--- Multisieve/pronounce_cr.py
+++ Multisieve/pronounce_cr.py
@@ -18,7 +18,7 @@ def pronoun_sieve(obj_document):
     # - func 找到代词表述
     pronoun_mentions = []
     for mention in obj_document.lst_mentions:
-        if mention.chinese_word in ConstantVariable.pronouns:
+        if mention.chinese_word in ConstantVariable.third_person_pronouns:
             pronoun_mentions.append(mention)
 
 
@@ -26,10 +26,25 @@ def pronoun_sieve(obj_document):
     for p_m in pronoun_mentions:
         # candidate_mentions = [mention for mention in obj_document.lst_mentions[:p_m.mention_id]\
         #                       if fabs(mention.sent_id - p_m.sent_id) <= 3]
+
+        # 实验3 将代词与它前面的指代
+        ancedent_mention = obj_document.dic_mentions.get(p_m.mention_id - 1, None)
+        # if ancedent_mention:
+        #     obj_document.set_coref(ancedent_mention, p_m)
+
+
         candidate_mentions = get_candidate_mentions(obj_document, p_m)
 
+        for c_m in reversed(candidate_mentions):
+            if c_m.ner == 'PERSON':
+                ancedent_mention = c_m
+                break
+
+        if ancedent_mention:
+            obj_document.set_coref(ancedent_mention, p_m)
+
         # - func 从候选表述中依次进行判断
-        for candidate_m in candidate_mentions:
+        # for candidate_m in candidate_mentions:
 
             # - rule 如果动物属性相同（只有相同的适合相乘才可能等于1）
             # if candidate_m.animacy * p_m.animacy == 1:
@@ -44,9 +59,22 @@ def pronoun_sieve(obj_document):
 
 
             # 实验2： 仅使用动物属性
-            if candidate_m.animacy == p_m.animacy:# and \
-                obj_document.set_coref(candidate_m, p_m)
-                break
+            # if candidate_m.animacy == p_m.animacy:# and \
+            #     obj_document.set_coref(candidate_m, p_m)
+            #     break
+
+        # if not str(p_m.entity_id).startswith('E_'):
+        #     ancedent_mention = obj_document.dic_mentions.get(p_m.mention_id - 1, None)
+
+            # for c_m in reversed(candidate_mentions):
+            #     if c_m.ner == 'PERSON':
+            #         ancedent_mention = c_m
+            #         break
+
+            # if ancedent_mention:
+            #     obj_document.set_coref(ancedent_mention, p_m)
+
+
 
     return obj_document
 
